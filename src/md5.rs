4f72796c9dbc2b431931277c602359a574fc3075
Wadii Hajji <wadii@cardiologs.com>
2022-07-11 00:52:49 +0200

    feat(md5): Add tests from ruby/digest

--- src/md5.rs
+++ src/md5.rs
@@ -32,10 +32,44 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let mut digest = MD5::new();
-        digest.base("Hello".to_string());
-        println!("{:?}", MD5::new().reset());
-        assert_eq!(2, 2);
+    fn it_computes_md5_0() {
+        let digest = MD5::hexdigest("");
+        assert_eq!(digest, "d41d8cd98f00b204e9800998ecf8427e");
+    }
+
+    #[test]
+    fn it_computes_md5_1() {
+        let digest = MD5::hexdigest("a");
+        assert_eq!(digest, "0cc175b9c0f1b6a831c399e269772661");
+    }
+
+    #[test]
+    fn it_computes_md5_2() {
+        let digest = MD5::hexdigest("abc");
+        assert_eq!(digest, "900150983cd24fb0d6963f7d28e17f72");
+    }
+
+    #[test]
+    fn it_computes_md5_3() {
+        let digest = MD5::hexdigest("message digest");
+        assert_eq!(digest, "f96b697d7cb7938d525a2f31aaf161d0");
+    }
+
+    #[test]
+    fn it_computes_md5_4() {
+        let digest = MD5::hexdigest("abcdefghijklmnopqrstuvwxyz");
+        assert_eq!(digest, "c3fcd3d76192e4007dfb496cca67e13b");
+    }
+
+    #[test]
+    fn it_computes_md5_5() {
+        let digest = MD5::hexdigest("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+        assert_eq!(digest, "d174ab98d277d9f5a5611c2c9f419d9f");
+    }
+
+    #[test]
+    fn it_computes_md5_6() {
+        let digest = MD5::hexdigest("12345678901234567890123456789012345678901234567890123456789012345678901234567890");
+        assert_eq!(digest, "57edf4a22be3c955ac49da2e2107b67a");
     }
 }
